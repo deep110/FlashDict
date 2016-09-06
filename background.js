02d1976c476cb460032a3bea26dc515d65276f99
deep-154 <deepankar11093@gmail.com>
2016-09-07 04:38:33 +0530

    DictService api integrated

--- background.js
+++ background.js
@@ -4,8 +4,25 @@ chrome.commands.onCommand.addListener(function (command) {
         chrome.tabs.executeScript( {
 		  	code: "window.getSelection().toString();"
 			}, function(selection) {
-				console.log(selection.length);
-		  		alert(selection[0]);
+				console.log(selection[0]);
+				getMeaning("hello");
+		  		//alert(selection[0]);
 		});
     }
 });
+
+function getMeaning(word){
+
+	var url = "http://services.aonaware.com/DictService/DictService.asmx/Define?word="+String(word);
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+            console.log("request executed");
+            var a = xmlHttp.responseXML;
+            console.log(a);
+        }
+    }
+    xmlHttp.open("GET", url, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
