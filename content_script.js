c122e7a7c8bcd31babf60bdb2074488f18ed680c
Chirag Maheshwari <csoni111@gmail.com>
2016-09-17 20:34:42 +0530

    Added black overlay for showing meanings.

--- content_script.js
+++ content_script.js
@@ -1,17 +1,41 @@
-document.body.addEventListener("dblclick", function(){
+// Add bubble to the top of the page.
+var bubbleDOM = document.createElement('div');
+bubbleDOM.setAttribute('class', 'selection_bubble');
+document.body.appendChild(bubbleDOM);
+
+document.body.addEventListener("dblclick", function(e){
 	var selectedText = window.getSelection().toString().trim();
-	chrome.runtime.sendMessage({message: selectedText});
+	if (selectedText.length > 0) {
+    chrome.runtime.sendMessage({message: selectedText});
+    var y_pos = document.body.scrollTop + e.clientY;
+    var x_pos = document.body.scrollLeft + e.clientX;
+    renderBubble(x_pos, y_pos);
+  }
 });
+
+// Close the bubble when we click on the screen.
+document.addEventListener('mousedown', function (e) {
+  bubbleDOM.style.visibility = 'hidden';
+  bubbleDOM.innerHTML = "";
+}, true);
 
 
 chrome.runtime.onMessage.addListener(function(request, sender) {
-    if(!sender.tab){
-    	var result = request.meaning;
+  if(!sender.tab) {
+   var result = request.meaning;
+   bubbleDOM.innerHTML = result;
+    	// showDialog(result);
     	//result = result.replace(/(\r\n|\n|\r)/gm,"");
     	//console.log(result);
-    	alert(result);
+    	// alert(result);
     }
-});
+  });
+
+function renderBubble(mouseX, mouseY) {
+  bubbleDOM.style.top = (mouseY + 20) + 'px';
+  bubbleDOM.style.left = (mouseX - 150) + 'px';
+  bubbleDOM.style.visibility = 'visible';
+}
 
 // function showDialog(textContent){
 // 	var dialog = document.createElement("dialog");
