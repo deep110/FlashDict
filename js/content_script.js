47b53596fb3436f7794f9416b014f416b389ab75
Chirag Maheshwari <csoni111@gmail.com>
2016-09-27 12:42:47 +0530

    Formatting of content & more button added.

--- js/content_script.js
+++ js/content_script.js
@@ -1,9 +1,14 @@
-var $x=0,$y=0;
+$x=0,$y=0;
 
 // Add bubble to the top of the page.
-var $box = $("<div>", {"class": "box_overlay"});
+$box = $("<div>", {"class": "box_overlay"});
+$content = $("<div>",{"class": "meaning_content"});
+$moreButton = $("<div>",{"class": "more_button"});
+$moreLink = $("<a>",{"target":"_blank"});
+$moreButton.append($moreLink);
+$box.append($content);
+$box.append($moreButton);
 $("body").append($box);
-$box = $(".box_overlay");
 
 $("body").dblclick(function(e){
   var selectedText = window.getSelection().toString().trim();
@@ -16,15 +21,15 @@ $("body").dblclick(function(e){
 });
 
 // Close the bubble when we click on the screen.
-$("body :not(.box_overlay)").on("click", function () {
+$("body :not(.box_overlay, .box_overlay *)").on("click", function () {
+  changeContent('','');
   $box.removeClass("visible");
-  $box.html("");
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender) {
   if(!sender.tab) {
     if(request.active) {
-      $box.html(request.meaning);
+      changeContent(request.word, request.meaning);
     }
   }
 });
@@ -33,9 +38,21 @@ $box.bind("DOMSubtreeModified",function(){
   $box.addClass("visible");
   $x -= $(this).width()/2;
   $y -= $(this).height() + 50;
-  if($x<0)
-    $x=0;
-  if($y<0)
-    $y += $(this).height() + 60;
+  if($x<0) $x=0;
+  if($y<0) $y += $(this).height() + 60;
   $(this).offset({top:($y>0?$y:0),left:$x});
 });
+
+function changeContent(word, meaning) {
+  if(meaning.length == 0) {
+    $moreButton.hide();
+  } else {
+    if(meaning.length > 500) {
+      meaning = meaning.substring(0,500).concat('...');
+    }
+    meaning = '<pre>' + meaning + '</pre>';
+    $moreLink.attr('href','http://www.google.com/search?q='+encodeURIComponent(word));
+    $moreButton.show();
+  }
+  $content.html(meaning);
+}
