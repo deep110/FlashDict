d4b3ce07803ca3de995b95d0e5f78130127e6001
iamharsshit <harshit.bansalec@gmail.com>
2017-12-19 23:13:04 +0530

    add mediawiki api and parse result

--- js/background.js
+++ js/background.js
@@ -61,8 +61,75 @@ function parseXmlData(word,xmlDoc){
 	if(meaning==null) {
 		if(alt!=null) {
 			meaning = alt;
-		}else meaning = "No meanings found";
+			result["meaning"] = meaning;
+			result["active"] = active;
+			sendResult(result);
+		}else {
+			searchWikipedia(word);
+		}
+	}else {	
+		result["meaning"] = meaning;
+		result["active"] = active;
+		sendResult(result);
 	}	
+}
+
+
+function searchWikipedia(word){
+	var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ word +"&format=json";
+	$.ajax({
+			url: url,
+			type: 'GET',
+			contentType: "application/json; charset=utf-8",
+			async: false,
+        	dataType: "json",
+			success: function(data, status, jqXHR) {
+        		parseWikiXmlData(word, data);
+        	}
+		}
+	);   
+}
+
+function parseWikiXmlData(word,data){
+
+	var horizontal_space = "     ";
+
+	var meaning = word + "\n" + horizontal_space;
+
+	if (data[1].length == 0){
+		meaning = "No meaning found!!"
+	}else{
+		$.each( data[1], function( key, value ) {
+
+			// display the related word
+			var index = key + 1;
+			meaning = meaning + index.toString() + ": " + data[1][key] + ": ";
+
+			// indent the meaning of the word obtained
+			var word_meaning = data[2][key];
+			var words_array = word_meaning.split(" ");
+			
+			// display the first line of the meaning found
+			var words_first_line = 6;
+			var first_line_words = words_array.slice(0, words_first_line);
+			var first_line_string = first_line_words.join(" ");
+	  		meaning = meaning + first_line_string + "\n" + horizontal_space;
+
+			var rest_line_words = words_array.slice(words_first_line, words_array.length);
+			var words_per_line = 9;
+
+			// display the remaining lines of the meaning
+			for (var i = 0; i < rest_line_words.length; i += words_per_line){
+	  			var limit = Math.min(i + words_per_line, rest_line_words.length);
+	  			var one_line_words = rest_line_words.slice(i, limit);
+	  			var one_line_string = one_line_words.join(" ");
+	  			meaning = meaning + one_line_string + "\n" + horizontal_space;
+	  		}
+		});
+	}
+
+	result = {};
+	result["word"] = word;	
 	result["meaning"] = meaning;
 	result["active"] = active;
 	sendResult(result);
@@ -73,7 +140,3 @@ function sendResult(result){
 		chrome.tabs.sendMessage(tabs[0].id, result);
 	});
 }
-
-
-
-
